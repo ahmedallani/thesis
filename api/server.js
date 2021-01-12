d4da1a72ca1cbf210fa3cee9a9e04268cc8dec84
ahmedallani <allania7med1@gmail.com>
2021-01-12 22:23:15 +0100

    pwa

--- api/server.js
+++ api/server.js
@@ -1,22 +1,28 @@
+const express = require("express");
+require('dotenv').config()
+const path = require("path");
 const { User } = require("./db/models/users");
 
 const port = process.env.PORT || 3000;
-const isProd = process.env.NODE_ENV === "production";
-
+let isProd = process.env.NODE_ENV === "production";
 const app = require("./index");
+if (isProd) {
+  let pth = path.join(__dirname, "../dist");
+  app.use(express.static(pth));
+} else {
+  const { Nuxt, Builder } = require("nuxt");
 
-const { Nuxt, Builder } = require("nuxt");
-// We instantiate Nuxt with the options
-const config = require("../nuxt.config.js");
-config.dev = !isProd;
+  const config = require("../nuxt.config.js");
+  config.dev = !isProd;
 
-const nuxt = new Nuxt(config);
-// Start build process in dev mode
-if (config.dev) {
-  const builder = new Builder(nuxt);
-  builder.build();
+  const nuxt = new Nuxt(config);
+
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    builder.build();
+  }
+  app.use(nuxt.render);
 }
-app.use(nuxt.render);
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 io.on("connection", socket => {
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 let obj = require("vuetify/es5/util/colors");
 let colors = obj.default;
 module.exports = {
+  server:{
+    host:"0.0.0.0"
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   serverMiddleware: ["~/api/index.js"],
   head: {
@@ -35,8 +38,21 @@ module.exports = {
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
     "mdbvue/nuxt",
-    "nuxt-leaflet"
+    "nuxt-leaflet",
+    '@nuxtjs/pwa'
   ],
+  pwa: {
+    meta: {
+      title: 'My PWA',
+      author: 'Me'
+    },
+    manifest: {
+      name: 'Nuxt.js PWAs are so easy',
+      short_name: 'Nuxt.js PWA',
+      lang: 'en'
+    }
+  },
+
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
